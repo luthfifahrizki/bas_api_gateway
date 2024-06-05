13601fe42d04b6865598bac20bc0f932d41e4936
luthfifahrizki <luthfifahrizki02@gmail.com>
2024-06-05 21:41:22 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"api_gateway/usecase"
-	"fmt"
+	"api_gateway/handler"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	login := usecase.NewLogin()
-	auth := login.Autentikasi("admin", "admin123")
-	fmt.Println(auth)
+	r := gin.Default()
+
+	accountRoute := r.Group("/account")
+	accountRoute.GET("/get", handler.NewAccount().GetAccount)
+	accountRoute.POST("/create", handler.NewAccount().CreateAccount)
+	accountRoute.PATCH("/update/:id", handler.NewAccount().UpdateAccount)
+	accountRoute.DELETE("/remove/:id", handler.NewAccount().RemoveAccount)
+	accountRoute.POST("/getbalance", handler.NewAccount().GetBalance)
+
+	transactionRoute := r.Group("/transaction")
+	transactionRoute.POST("/transferbank", handler.NewTransaction().TransferBank)
+
+	authRoute := r.Group("/auth")
+	authRoute.POST("/login", handler.NewAuth().Login)
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
